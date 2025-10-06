1405ea70783737fea403fe6bda41b5b72a6f7e0a
mdsharia4613 <mdsharia4613@gmail.com>
2025-10-06 22:53:26 +0600

    problem 16

---
+++ part-2.js
@@ -0,0 +1,37 @@
+// problem- 12
+
+// problem- 13
+const number = -24;
+
+if(number < 0){
+    console.log("absolute vakue:", number * -1);
+}
+else{
+    console.log('Absolute vakue:', number);
+}
+
+// problem- 14
+
+const a = 30;
+const b = 20;
+const c = 40;
+
+if((a + b) > c && (a + c) > b && (b + c) > a){
+    console.log('Triangle is possible');
+}
+else{
+    console.log('Triangle is not possible');
+}
+
+// problem 15
+
+const buy = 200;
+const sell = 240;
+if(sell > buy){
+    console.log('profit')
+}
+else{
+    console.log('loss')
+}
+
+// problem 16
